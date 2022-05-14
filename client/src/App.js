fd9ce88011eeabdbc7a148a0cd0bdf94f205d27c
Jorge Rojas <exxscher@gmail.com>
2022-05-14 10:51:33 -0400

    Se actualizan rutas, se añade: enlaces escuela/detalle > detalle/formulario, botones de navegación.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from './views/Home';
 import Main from './views/Main';
-import Detalle from './views/Detalle';
+import SchoolDetails from './components/SchoolDetails';
 import RegisterLogin from './views/RegisterLogin';
+import ReviewForm from './components/ReviewForm';
 import { UserProvider } from './contexts/userContext';
 
 function App() {
@@ -13,18 +14,19 @@ function App() {
         <Router>
           <div className="header-container">
             <div className='header'>
-              <h1>Evalúa</h1>
+              <h1><Link to='/'>Evalúa</Link></h1>
               <nav>
                 <Link to={"/"}>Inicio</Link>
-                <Link to={"/main"}><i className="fa-solid fa-magnifying-glass"></i> Evaluaciones</Link>
+                <Link to={"/search"}><i className="fa-solid fa-magnifying-glass"></i> Evaluaciones</Link>
                 <Link to={"/registerLogin"}>Inicia sesión / Regístrate</Link>
               </nav>
             </div>
           </div>
           <Routes>
             <Route path="/" element={<Home></Home>} />
-            <Route path="/main" element={<Main></Main>} />
-            <Route path="/detalle" element={<Detalle></Detalle>} />
+            <Route path="/search" element={<Main></Main>} />
+            <Route path="/reviews/:id" element={<SchoolDetails/>} />
+            <Route path="/create-review/:id" element={<ReviewForm/>} />
             <Route path="/registerLogin" element={<RegisterLogin></RegisterLogin>} />
           </Routes>
         </Router>
