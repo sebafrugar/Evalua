8040fce39c2a5c9932ccb5c33a891b5f9a7134ff
Lorena-Concha <kleyna@gmail.com>
2022-05-16 16:33:00 -0400

    desaparece botonera al logearse

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from './views/Home';
 import Main from './views/Main';
-import Detalle from './views/Detalle';
+import SchoolDetails from './components/SchoolDetails';
 import RegisterLogin from './views/RegisterLogin';
+import ReviewForm from './components/ReviewForm';
 import { UserProvider } from './contexts/userContext';
 
 function App() {
@@ -11,13 +12,20 @@ function App() {
     <div className="App">
       <UserProvider>
         <Router>
+          <div className="header-container">
+                <Header/>
+          </div>
           <Routes>
             <Route path="/" element={<Home></Home>} />
-            <Route path="/main" element={<Main></Main>} />
-            <Route path="/detalle" element={<Detalle></Detalle>} />
+            <Route path="/search" element={<Main></Main>} />
+            <Route path="/reviews/:id" element={<SchoolDetails/>} />
+            <Route path="/create-review/:id" element={<ReviewForm/>} />
             <Route path="/registerLogin" element={<RegisterLogin></RegisterLogin>} />
           </Routes>
         </Router>
+        <footer>
+          Todos los derechos reservados © 2022
+        </footer>
       </UserProvider>
     </div>
   );
