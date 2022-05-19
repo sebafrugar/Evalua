8a3cf9ced9e9f78aaab1e3f563086e25e51e7f78
Jorge Rojas <exxscher@gmail.com>
2022-05-18 21:49:20 -0400

    Correcciones ortográficas

--- client/src/components/BannerInfo.js
+++ client/src/components/BannerInfo.js
@@ -14,7 +14,7 @@ const BannerInfo = () => {
                 <div className='info-contents'>
                     <div>
                         <p>Evalúa, es una plataforma que califica las escuelas como lugar de trabajo.</p>
-                        <p>Nuestra misión es entregar una herramienta para visivilizar realidades que se encuentan ocultas en el diario vivir de la comunidad educativa, permitiendo destacar a aquellos empleadores donde sus pares, tales como: docentes, auxiliares, Psicopedagogos, Personal de alimentación, etc, recomiendan trabajar.</p>
+                        <p>Nuestra misión es entregar una herramienta para visibilizar realidades que se encuentan ocultas en el diario vivir de la comunidad educativa, permitiendo destacar a aquellos empleadores donde sus pares, tales como: docentes, auxiliares, Psicopedagogos, Personal de alimentación, etc, recomiendan trabajar.</p>
                         <p>Sabemos que los equipos de trabajo que cuentan con herramientas adecuadas, trato respetuoso, etica profesional pueden influir directamente en los estudiantes, permitiendo una mejor educación y mejor bienestar emocional para nuestros estudiantes.</p>
                     </div>
                     <div>
